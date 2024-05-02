2aaafa37aef5d44a654149f22811727c588cd0d7
Mike Parton <mike.parton@budgetly.com.au>
2024-05-02 22:45:01 +0800

    Add initStateAPI

--- src/config.js
+++ src/config.js
@@ -10,12 +10,13 @@
 
 export const appConfig = {
   // MODULE 1- BACKEND
-  initStateAPI: '', // e.g. https://12abcdef89.execute-api.us-west-2.amazonaws.com/Prod/InitState/
+  initStateAPI:
+    'https://c7ym7aswgi.execute-api.ap-southeast-2.amazonaws.com/Prod/InitState/',
   // MODULE 2 - REALTIME
   iot: {
     poolId: '', // e.g. 'us-west-2:1abcdef-1234-abcd-1234-abcde123456'
     host: '', // e.g. 'ab12ab12abcde.iot.us-east-1.amazonaws.com'
-    region: '' // e.g. 'us-west-1'
+    region: '', // e.g. 'us-west-1'
   },
   // MODULE 3 - PHOTOS
   photoUploadURL: '', // e.g. https://12abcdfg89.execute-api.us-west-2.amazonaws.com/Prod/Upload
@@ -23,12 +24,14 @@ export const appConfig = {
   // Don't modify anything below this comment!
   //
   images: {
-    parkMapURL: 'https://d15l97sovqpx31.cloudfront.net/images/theme-park-map-large.jpg',
-    logoURL: 'https://da-public-assets.s3.amazonaws.com/innovator-island/innovator-island-logo-150px.png'
+    parkMapURL:
+      'https://d15l97sovqpx31.cloudfront.net/images/theme-park-map-large.jpg',
+    logoURL:
+      'https://da-public-assets.s3.amazonaws.com/innovator-island/innovator-island-logo-150px.png',
   },
   icons: {
     restroom: 'https://d15l97sovqpx31.cloudfront.net/icons/icon-restroom.png',
     dining: 'https://d15l97sovqpx31.cloudfront.net/icons/icon-dining.png',
-    atm: 'https://d15l97sovqpx31.cloudfront.net/icons/icon-atm.png'
-  }
-}
+    atm: 'https://d15l97sovqpx31.cloudfront.net/icons/icon-atm.png',
+  },
+};
